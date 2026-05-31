2329be3113aabfad420847b021a30176bde8ee80
Mikhail Smirnov <mishru01876@gmail.com>
2026-05-31 21:57:00 +0300

    фикс ошибок

---
+++ AtsBillingSystem.UI/Views/CallLogView.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using AtsBillingSystem.UI.ViewModels;
+
+namespace AtsBillingSystem.UI.Views
+{
+    public partial class CallLogView : UserControl
+    {
+        public CallLogView()
+        {
+            InitializeComponent();
+
+            // Архитектурно правильный способ связать View и ViewModel без нарушения MVVM:
+            // Как только интерфейс отрисовался, мы просим ViewModel загрузить первичные данные.
+            this.Loaded += OnLoaded;
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            if (DataContext is CallLogViewModel viewModel)
+            {
+                // Запускаем безопасную инициализацию
+                viewModel.LoadInitialDataCommand.Execute(null);
+            }
+        }
+    }
+}
