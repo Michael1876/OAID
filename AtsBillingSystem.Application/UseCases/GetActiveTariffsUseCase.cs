74c565dc734d20058c751b4253e1ff994c57bc80
Mikhail Smirnov <mishru01876@gmail.com>
2026-06-05 00:16:56 +0300

    -

--- AtsBillingSystem.Application/UseCases/GetActiveTariffsUseCase.cs
+++ AtsBillingSystem.Application/UseCases/GetActiveTariffsUseCase.cs
@@ -1,12 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
+using AtsBillingSystem.Domain.Interfaces.Repositories;
+using AtsBillingSystem.Domain.Interfaces.UseCases;
+using AtsBillingSystem.Domain.Models;
 
 namespace AtsBillingSystem.Application.UseCases
 {
-    internal class GetActiveTariffsUseCase
+    public class GetActiveTariffsUseCase : IGetActiveTariffsUseCase
     {
+        private readonly ITariffRepository _tariffRepository;
+
+        public GetActiveTariffsUseCase(ITariffRepository tariffRepository)
+        {
+            _tariffRepository = tariffRepository;
+        }
+
+        public async Task<IEnumerable<DomainTariff>> ExecuteAsync()
+        {
+            // Получаем только активные тарифы (без архивных) через репозиторий
+            return await _tariffRepository.GetActiveAsync();
+        }
     }
 }
