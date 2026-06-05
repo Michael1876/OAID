a6f6768352dd7150ecc1d732ad87b43a3cfe2cb8
Mikhail Smirnov <mishru01876@gmail.com>
2026-06-05 18:48:44 +0300

    фикс экрана биллинга

---
+++ AtsBillingSystem.UI/Views/BillingProcessingView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace AtsBillingSystem.UI.Views
+{
+    public partial class BillingProcessingView : UserControl
+    {
+        public BillingProcessingView()
+        {
+            InitializeComponent();
+        }
+    }
+}
